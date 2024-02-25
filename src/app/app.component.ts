c2154e1991c68f10a2a7b2603f1e34b99d0e991d
naohiko560 <private.mail.nao@gmail.com>
2024-02-25 10:10:59 +0900

    レッスン 2: Home コンポーネントの作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img src="/assets/logo.svg" alt="logo" class="brand-logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
